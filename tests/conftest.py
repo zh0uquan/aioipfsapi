ca7397b2ae33a9e3b949c2f9039fa4681fd813b9
zh0uquan <zhouquan0x16@gmail.com>
2018-02-13 14:15:34 +0100

    refresh conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,6 @@ import asyncio
 import pytest
 
 import aioipfsapi
-from .server import IPFSServer, app
 
 
 @pytest.fixture()
@@ -18,22 +17,8 @@ def event_loop():
 
 
 @pytest.fixture()
-def ipfs_server(request):
-    ipfs_server = IPFSServer(
-        aioipfsapi.DEFAULT_HOST, aioipfsapi.DEFAULT_PORT, app
-    )
-    ipfs_server.start()
-
-    request.addfinalizer(ipfs_server.stop)
-
-    return ipfs_server
-
-
-@pytest.fixture()
 def ipfs_client(event_loop):
 
-    client = aioipfsapi.AioClient(loop=event_loop
-
-    )
+    client = aioipfsapi.AioClient(loop=event_loop)
 
     return client
